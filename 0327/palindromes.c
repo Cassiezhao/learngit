6e3eb6c98a11c8d3c8802084f58c98aa21395b39
cassie <ylvh1314@163.com>
2017-03-27 21:12:54 +0800

    add 0327

---
+++ 0327/palindromes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+    char s[100];  // 存放输入的字符串 
+    int i, j, n;
+    printf("输入字符串：");
+    gets(s);
+    
+    n=strlen(s);
+    for(i=0,j=n-1;i<j;i++,j--)
+        if(s[i]!=s[j]) break;
+        if(i>=j)
+            printf("是回文串\n");
+        else
+            printf("不是回文串\n");
+
+    return 0;
+}
